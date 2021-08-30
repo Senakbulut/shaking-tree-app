21ca565231c00201f3c485f2940493ef6893b52d
Sena Akbulut <34038741+Senakbulut@users.noreply.github.com>
2021-08-30 15:07:05 +0300

    comment lines added

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -6,22 +6,19 @@ function Tree(props) {
 
     const [shaking, setshake] = useState(false)
  
-
-    const ShakeTree = () =>{
+    const ShakeTree = () =>{ //Synchronization was achieved by specifying the tree shaking that will last 3 seconds with the setTimeout function and then calling the apple falling animation with props.
         setshake(true);
         setTimeout(()=>setshake(false), 3000);
         setTimeout(() => {
-            props.fall()}, 3000);
-        
-    }
-    
+            props.fall()}, 3000);  
+    }   
     return (
         <div>
             <div className="containerTree">
                     <img src="../../../public/images/tree.png" alt="" className={`treeImg ${shaking ? 'shake' : null }`}  />
             </div>
-           
               <ShakeButton shake ={ShakeTree}/>
+              /*prepared function sent to button*/
         </div>
     )
 }
