b4a17c438c5b8487f867bd4a046032987349cd70
Sena Akbulut <34038741+Senakbulut@users.noreply.github.com>
2021-08-30 15:01:09 +0300

    comment lines added

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -4,14 +4,15 @@ import Apple from '../Apple/Apple'
 function Basket() {
     const [basketApple, setBasketApple] = useState(false);
 
-    const visibleApple = ()=>{
+    const visibleApple = ()=>{ //The function prepared for the apples in the basket to fall 1 second after the apples fall
        
         setBasketApple(true);
         setTimeout(()=>setBasketApple(false), 6000);
     }
     return (
         <div>
-            <Apple visible={visibleApple}/>
+        /*Function is sent to Apple component so it can be used with props*/
+            <Apple visible={visibleApple}/> 
             <div className="containerBasket">
                 <img src="../../../public/images/basket.png" alt="" className="basketImg" />
                 <img src="../../../public/images/apple.png" alt=""  className={`basketapple1 ${basketApple ? 'vBasketapple' : null }`}/>
