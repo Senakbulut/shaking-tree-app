e0d7905669ed2a079dc8cce8186597418f6272e4
Sena Akbulut <34038741+Senakbulut@users.noreply.github.com>
2021-08-30 13:56:29 +0300

    command lines added

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //extra css plugin
 
 module.exports = {
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js',
+    output: { //options related to how webpack emits results
+        path: path.join(__dirname, '/dist'), //the target directory for all output files and absolute path
+        filename: 'index.bundle.js', //the filename template for entry chunks
     },
     devServer: {
-        port: 3000,
+        port: 3000, //port number to development server
         watchContentBase: true,
     },
     module: {
-        rules: [
+        rules: [ //rules for modules 
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+                test: /\.(js|jsx)$/, //For files ending in .js
+                exclude: /node_modules/, // node_modules are not included
+                use: { // options for the loader 
+                    loader: 'babel-loader' //the loader which should be applied, it'll be resolved relative to the context
                 }
             },
             {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
+                test: /\.scss$/, //For files ending in .scss
+                use: [ 
+                    MiniCssExtractPlugin.loader, // loaders for scss
                     'css-loader',
                     'sass-loader',
                 ],
             },
             {
-                test:/\.sass$/,
+                test:/\.sass$/, //For files ending in .sass
                 use: [
-                    'style-loader',
+                    'style-loader', //loaders for sass
                     'css-loader',
                     'sass-loader'
                 ]
             },
             {
-                test:/\.css$/,
+                test:/\.css$/, //For files ending in .css
                 use: [
-                    'style-loader',
+                    'style-loader', //loaders for css
                     'css-loader',
                     'sass-loader'
                 ]
             }
         ]
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [new MiniCssExtractPlugin()], //additional plugins
 };
