f583b6355c900a66cae41b16496eca3675a2c74a
Sena Akbulut <34038741+Senakbulut@users.noreply.github.com>
2021-08-30 15:03:50 +0300

    comment lines added

--- src/components/ShakeButton/ShakeButton.js
+++ src/components/ShakeButton/ShakeButton.js
@@ -5,6 +5,7 @@ function ShakeButton(props) {
 
     return (
         <div>
+        /*By accessing the shake variable sent by the Tree component with props, the defined function is activated by clicking the button.*/
             <div className="containerButton" >
                 <div className="wrapperButton" onClick={props.shake} >
                 <a href="#" className="buttonCircle">Shake</a>
