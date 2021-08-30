026d6215eaf80929571c2281a5f63a315c392ae1
Sena Akbulut <34038741+Senakbulut@users.noreply.github.com>
2021-08-30 14:42:04 +0300

    command lines added

--- src/components/Apple/Apple.js
+++ src/components/Apple/Apple.js
@@ -4,27 +4,27 @@ import Tree from '../Tree/Tree'
 
 function Apple(props) {
 
-    const [fallApple1, setFallApple1] = useState(false)
+    const [fallApple1, setFallApple1] = useState(false) //controllable variables defined with useState for apple animations
     const [fallApple2, setFallApple2] = useState(false)
     const [fallApple3, setFallApple3] = useState(false)
 
-    const fallApples = () =>{
+    const fallApples = () =>{ //apples fall animation function
          setFallApple1(true); setFallApple2(true); setFallApple3(true);
-         setTimeout(()=>setFallApple1(false),5000); 
+         setTimeout(()=>setFallApple1(false),5000); //animations defined at different seconds from each other have been set
          setTimeout(()=>setFallApple2(false),6000);
          setTimeout(()=>setFallApple3(false),7000);
-         setTimeout(()=>{
+         setTimeout(()=>{ //It is stated that after the apples fall, they access the function that provides the visibility of the apples in the basket with props.
              props.visible()}, 4000);
     }
 
     return (
         <div>
            <Tree fall ={fallApples}/>
-           
+           <!-- It is stated that if the variable that provides control with useState is true, the class defined in the sass file should be added.-->
             <div className="containerApple">
                     <img src="../../../public/images/apple.png" alt="" className={`appleImg1 `}/>
                     <img src="../../../public/images/apple.png" alt="" className={`appleImg2 `} />
-                    <img src="../../../public/images/apple.png" alt="" className={`appleImg3 ${fallApple1 ? 'fallAppl1' : null }`} />
+                    <img src="../../../public/images/apple.png" alt="" className={`appleImg3 ${fallApple1 ? 'fallAppl1' : null }`} /> 
                     <img src="../../../public/images/apple.png" alt="" className={`appleImg4 ${fallApple2 ? 'fallAppl2' : null }`} />
                     <img src="../../../public/images/apple.png" alt="" className={`appleImg5 `} />
                     <img src="../../../public/images/apple.png" alt="" className={`appleImg6 `} />
